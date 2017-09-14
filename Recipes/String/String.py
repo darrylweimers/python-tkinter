44f67ccea7105f5b6df67956d166309d2f608f7a
darrylweimers <30273976+darrylweimers@users.noreply.github.com>
2017-09-14 16:23:50 -0400

    String.py

--- Recipes/String/String.py
+++ Recipes/String/String.py
@@ -16,3 +16,25 @@ def test_convert_a_string_to_raw_string_2():
     string = "\n\n\n"
     raw_string = convert_a_string_to_raw_string(string)
     print(raw_string.count(r'\n'))
+
+    
+#remove all occurences of string 
+string = r"Types.h\n\n\nKeepThisPart\n\n\nKeep-this-part"
+index = string.find(r'\n\n\n')
+print(string)
+# for index in range(0, index):
+#     print(string[index], end="")
+# for index in range(index + len(r'\n\n\n') + 1, len(string)):
+#     print(string[index], end="")
+
+print(string[0:index], end='')
+print(string[index + len(r'\n\n\n') + 1: len(string)], end='')
+
+
+print()
+print(string.count(r'\n\n\n'))
+
+
+
+
+#def remove_occurence_of(string='')
