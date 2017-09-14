012a2a416de7656d3063f369e0afcc9b94afc9fd
darrylweimers <30273976+darrylweimers@users.noreply.github.com>
2017-09-14 15:44:55 -0400

    String.py
    Revision

--- Recipes/String/String.py
+++ Recipes/String/String.py
@@ -1,13 +1,17 @@
 
-
 def convert_a_string_to_raw_string(string=''):
     raw_string = "%r" % string
     return raw_string
 
-def test_convert_a_string_to_raw_string():
+def test_convert_a_string_to_raw_string_1():
     string = "Convert\ta string to \n raw \tstring"
     print("Raw string:")
     print(convert_a_string_to_raw_string(string))
     print()
     print("String:")
     print(string)
+
+def test_convert_a_string_to_raw_string_2():
+    string = "\n\n\n"
+    raw_string = convert_a_string_to_raw_string(string)
+    print(raw_string.count(r'\n'))
