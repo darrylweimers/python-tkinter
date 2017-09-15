abf52f05d3515aabec47168be99e5093cd61f999
darrylweimers <30273976+darrylweimers@users.noreply.github.com>
2017-09-15 09:18:27 -0400

    Update Print.py

--- Recipes/Print/Print.py
+++ Recipes/Print/Print.py
@@ -6,3 +6,6 @@ print('string', end="")
 #print a range of numbers ( not including 5) 
 for number in range(1, 5):
   print(number)
+
+# print   
+print("Print", "strings", "with", "white", "spaces")
